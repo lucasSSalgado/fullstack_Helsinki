b5f0d020b688d2d0602c91637298a213df62b86f
lucasSSalgado <71613641+lucasSSalgado@users.noreply.github.com>
2023-12-16 13:33:04 -0300

    end of project

--- part2/phonebook/src/PersonForm.jsx
+++ part2/phonebook/src/PersonForm.jsx
@@ -2,13 +2,13 @@ export default function PersonForm(props) {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-            name: <input value={props.newName} onChange={(e) => props.setNewName(e.target.value)}/>          
+                name: <input value={props.newName} onChange={(e) => props.setNewName(e.target.value)}/>          
             </div>
             <div>
-            number: <input value={props.newNumber} onChange={(e) => props.setNewNumber(e.target.value)} />
+                number: <input value={props.newNumber} onChange={(e) => props.setNewNumber(e.target.value)} />
             </div>
             <div>
-            <button type="submit">add</button>
+                <button type="submit">add</button>
             </div>
         </form>
     )
