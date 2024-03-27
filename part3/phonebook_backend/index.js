8f950ade66348cdf0f2a9cce3224ea8ee352f792
lucasSSalgado <lucssslucsss@gmail.com>
2024-03-27 11:07:26 -0300

    first part3 backend

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let notes = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        res.json(note) 
+        return
+    }
+    else res.status(404).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const note = req.body
+    if (!note.name || !note.number) {
+        return res.status(400).json({ 
+            error: 'name or number missing' 
+        })
+    }
+
+    if (notes.find(n => n.name === note.name)) {
+        return res.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    }
+
+    const id = Math.floor(Math.random() * 1000000)
+    const newNote = { id, ...note }
+    notes.push(newNote)
+    return res.json(newNote)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+    res.json(notes)
+})
+
+app.get('/info', (req, res) => {
+    let htmlResp = 
+    `
+        <p>Phonebook has info for ${notes.length} people</p>
+        <p>${new Date()}</p>
+    `
+    res.send(htmlResp)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
