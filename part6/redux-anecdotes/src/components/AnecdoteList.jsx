c9b0344f0fd1d1603abf4b1c01dc8b0ed49eaf7a
lucasSSalgado <71613641+lucasSSalgado@users.noreply.github.com>
2024-05-02 13:08:32 -0300

    filter reducer

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,22 +2,33 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => state.anecdote)
     const dispatch = useDispatch()
 
     const orderByLike = (anecdotesArray) => {
         return anecdotesArray.sort((a, b) => b.votes - a.votes)
     }
 
-    let odered = orderByLike(anecdotes)
+    let ordered = orderByLike(anecdotes)
 
     const vote = (id) => {
         dispatch(voteAnecdote(id))
     }
 
+    const filtered = useSelector(state => {
+        if (state.filter === 'ALL') {
+          return ordered;
+        } 
+        else {
+          return ordered.filter(anecdote =>
+            anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+          );
+        }
+    });
+
     return (
         <div>
-            {odered.map(anecdote =>
+            {filtered.map(anecdote =>
                 <div key={anecdote.id}>
                 <div>
                     {anecdote.content}
