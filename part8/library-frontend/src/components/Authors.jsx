d3be0fc2652bab8c6c39443b3a26bdeb4c4c2508
lucasSSalgado <71613641+lucasSSalgado@users.noreply.github.com>
2024-05-14 20:18:54 -0300

    refactories diectories and end of part b

---
+++ part8/library-frontend/src/components/Authors.jsx
@@ -0,0 +1,41 @@
+import {  useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries/query'
+import EditAuthorForm from './EditAuthorForm'
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {result.data.allAuthors.map((a) => (
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <EditAuthorForm authors={result.data.allAuthors} />
+    </div>
+  )
+}
+
+export default Authors
---
+++ part8/library-frontend/src/components/EditAuthorForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import { useMutation } from "@apollo/client"
+import { EDIT_AUTHOR, ALL_AUTHORS } from "../queries/query"
+
+
+const EditAuthorForm = ({ authors }) => {
+    const [name, setName] = useState('')
+    const [born, setBorn] = useState('')
+
+    const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
+        refetchQueries: [ { query: ALL_AUTHORS } ]
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name)
+        editAuthor({variables: { name, setBornTo: Number(born) }})
+        setBorn('')
+        setName('')
+    }
+
+    return (
+        <div>
+            <h2>Set Birthdate</h2>
+            <form onSubmit={ handleSubmit }>
+                <select value={name} onChange={({ target }) => setName(target.value)}> 
+                    { authors.map(a => {
+                        return <option key={a.id} value={a.name}>{a.name}</option> 
+                    }) }
+                </select> <br />
+                <input type="number" name="born" value={born} onChange={({ target }) => setBorn(target.value)} /> <br />
+                <button>Update author</button>
+            </form>
+        </div>
+    )
+}
+
+export default EditAuthorForm
